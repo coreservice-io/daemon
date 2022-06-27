ca913771a09620e32da8b918fee92c80985e44cf
goodboy3 <zhangzhengbo@hotmail.com>
2022-06-27 14:22:16 +0800

    commit

---
+++ autobuild.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright 2022 Daqnext Foundation Ltd.
+
+rm -f -R ./build
+mkdir build
+
+#todo mac is for dev, not for pro
+#echo "Compiling MAC 64bit version"
+GOOS=darwin GOARCH=amd64 "./build/service-darwin-amd64"
+
+echo "Compiling Linux 64bit version"
+GOOS=linux GOARCH=amd64  go build -a -o "./build/service-linux-amd64"
+
+echo "Compiling ARM64 version"
+GOOS=linux GOARCH=arm64   go build -a -o "./build/service-linux-arm64"
